1bd79079f4b4f93c177ae84fc9098bee2108b56d
billgsm <ga_bilou@hotmail.com>
2022-12-19 14:23:16 +0100

    Update .config/nvim-packer/plugin/packer_compiled.lua

--- .config/nvim-packer/plugin/packer_compiled.lua
+++ .config/nvim-packer/plugin/packer_compiled.lua
@@ -112,7 +112,7 @@ _G.packer_plugins = {
   },
   harpoon = {
     loaded = true,
-    path = "/home/bilel/.local/share/nvim/site/pack/packer/start/harpoon",
+    path = my_home_dir .. "/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/ThePrimeagen/harpoon"
   },
   ["lsp-zero.nvim"] = {
