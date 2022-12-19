f92094b3355548536ce69fb1f5e442223ecbee45
billgsm <ga_bilou@hotmail.com>
2022-12-19 14:22:08 +0100

    Update .config/nvim-packer/plugin/packer_compiled.lua

--- .config/nvim-packer/plugin/packer_compiled.lua
+++ .config/nvim-packer/plugin/packer_compiled.lua
@@ -163,7 +163,7 @@ _G.packer_plugins = {
   ["rose-pine"] = {
     config = { "\27LJ\2\0029\0\0\2\0\3\0\0056\0\0\0009\0\1\0'\1\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0" },
     loaded = true,
-    path = "/home/bilel/.local/share/nvim/site/pack/packer/start/rose-pine",
+    path = my_home_dir .. "/.local/share/nvim/site/pack/packer/start/rose-pine",
     url = "https://github.com/rose-pine/neovim"
   },
   ["telescope.nvim"] = {
